33680985437af5cf2b626d8ddfacd2cebf25a83c
Meris Williams <merisw@gmail.com>
2013-05-15 22:55:06 -0700

    Generate how-to app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HowTo::Application.config.secret_token = '7f5dfab5a4a2ada9cca0f0557ba356051148b5f4aac4a8b8bfbebc2028b37fb27469489261076f4faac3afd3313f9590dbf88ea7559f97b21332b3a81b4efac6'
